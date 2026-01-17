7b7e0b8d78c14db0232bf985ecaa16bfd82e9512
kyoh86 <me@kyoh86.dev>
2026-01-17 23:20:37 +0900

    Make snaptest

---
+++ snapcase/wzu1lhx4/golden.lua
@@ -0,0 +1,5 @@
+vim.fn.setline(1, {
+  "test for wzu1lhx4",
+})
+vim.cmd.copen()
+vim.cmd.wincmd("k")
---
+++ snapcase/wzu1lhx4/target.lua
@@ -0,0 +1,6 @@
+vim.fn.setline(1, {
+  "test for wzu1lhx4",
+})
+vim.cmd.copen()
+vim.cmd.new({ mods = { split = "topleft" } })
+vim.cmd.quit()
