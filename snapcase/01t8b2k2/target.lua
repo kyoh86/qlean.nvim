f540771ef055be5f90140b8e67f42f66c1bce597
kyoh86 <me@kyoh86.dev>
2026-01-21 00:05:49 +0900

    chore: comments on scenario

--- snapcase/01t8b2k2/target.lua
+++ snapcase/01t8b2k2/target.lua
@@ -5,12 +5,18 @@ require("qlean").setup({
 
 vim.go.hidden = true
 
+-- Make a new modified hidden buffer
 vim.cmd.new()
 vim.fn.setline(1, { "foobar" })
 vim.cmd.wincmd("c")
 
+-- Open a quickfix-window as a UI window (it'll not be kept)
 vim.cmd.copen()
+
+-- Close last kept window
+-- - It closes other windows, hidden buffer is found, Neovim stops quitting because it is not saved (E37)
 vim.cmd.wincmd("k")
 pcall(vim.api.nvim_cmd, { cmd = "quit" }, { output = true })
+
 vim.cmd.redraw()
 snap_done()
